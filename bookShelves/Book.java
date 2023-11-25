9ab9612bf694bd02715401e7a5cca63914547f82
joven0015 <jovencuritao06@gmail.com>
2023-11-25 21:59:36 +0800

    Book

--- bookShelves/Book.java
+++ bookShelves/Book.java
@@ -1,24 +1,30 @@
 package bookShelves;
 
 public class Book {
-    public Book(){
-        String title; String author; int yearPublished; double price;
-    public Book(String title, String author, int yearPublished, double price) {
-    this.title = title;
-    this.author = author;
-    this.yearPublished = yearPublished;
-    this.price = price;
+    String title;
+    String author;
+    int yearPublished;
+    double price;
+
+    public Book(String title, String author, int yearPublished, double price){
+        {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }
+
     }
 
     @Override
     public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", yearPublished=" + yearPublished +
-                ", price=" + price +
-                '}';
+
+        return
+
+                "title : " + title + "\n"+
+                "author : " + author + "\n"+
+                "yearPublished : " + yearPublished + "\n"+
+                "price : " + price +"\n";
 
     }
-
 }
--- bookShelves/Main.java
+++ bookShelves/Main.java
@@ -1,8 +1,10 @@
 package bookShelves;
-
+import bookShelves.*;
 public class Main {
-      Book book = new Book("book1","John Smith",2021,39.99);
-        Book book2 = new Book("book2","Python Basics",2019,29.99);
-        Book book3 = new Book("book3", "Michael Johnson", 2020, 49.99);
-        System.out.println(book+"\n"+book2+"\n"+book3);
+        public static void main(String[] args) {
+                Book book1 = new Book("Java Programming","John Smith",2021,39.99);
+                Book book2 = new Book("Python Basics","Python Basics",2019,29.99);
+                Book book3 = new Book("C++ Essentials", "Michael Johnson", 2020, 49.99);
+                System.out.println("Book1:"+"\n"+book1+"\n"+"Book2;"+"\n"+book2+"\n"+"Book3:"+"\n"+book3);
+        }
 }
