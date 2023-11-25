c010a77c3b45c79e883c014515bebf916dfe30f2
joven0015 <111104962+joven0015@users.noreply.github.com>
2023-11-25 13:20:23 +0000

    Book

--- bookShelves/Book.java
+++ bookShelves/Book.java
@@ -2,6 +2,22 @@ package bookShelves;
 
 public class Book {
     public Book(){
+        String title; String author; int yearPublished; double price;
+    public Book(String title, String author, int yearPublished, double price) {
+    this.title = title;
+    this.author = author;
+    this.yearPublished = yearPublished;
+    this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", yearPublished=" + yearPublished +
+                ", price=" + price +
+                '}';
 
     }
 
--- bookShelves/Main.java
+++ bookShelves/Main.java
@@ -1,4 +1,8 @@
 package bookShelves;
 
 public class Main {
+      Book book = new Book("book1","John Smith",2021,39.99);
+        Book book2 = new Book("book2","Python Basics",2019,29.99);
+        Book book3 = new Book("book3", "Michael Johnson", 2020, 49.99);
+        System.out.println(book+"\n"+book2+"\n"+book3);
 }
